e5de2e232b36c9573bbcd7c1434f2cc506db20f6
Teague M <65279644+tmangan64@users.noreply.github.com>
2026-06-22 00:36:16 +0100

    Added flake.lock as well as dendritic pattern

--- flake.nix
+++ flake.nix
@@ -2,9 +2,15 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
 
   outputs = { self, nixpkgs }: {
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [ ./configuration.nix ];
+    nixosConfigurations = {
+      blackwall = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ ./hosts/blackwall ];
+      };
+
+      # Future hosts (laptop, desktop) will be added here:
+      # elysia = nixpkgs.lib.nixosSystem { ... };
+      # canto = nixpkgs.lib.nixosSystem { ... };
     };
   };
 }
---
+++ hosts/blackwall/default.nix
@@ -0,0 +1,72 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [
+      ./hardware.nix
+    ];
+
+  # Bootloader
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Hostname
+  networking.hostName = "blackwall";
+
+  # Networking
+  networking.networkmanager.enable = true;
+
+  # Static IP for SSH port forwarding
+  networking.interfaces.enp3s0 = {
+    useDHCP = false;
+    ipv4.addresses = [{
+      address = "192.168.1.66";
+      prefixLength = 24;
+    }];
+  };
+  networking.defaultGateway = "192.168.1.1";
+  networking.nameservers = [ "1.1.1.1" "8.8.8.8" ];
+
+  # Timezone
+  time.timeZone = "Europe/Jersey";
+
+  # Locale
+  i18n.defaultLocale = "en_GB.UTF-8";
+
+  # Keymap
+  services.xserver.xkb = {
+    layout = "gb";
+    variant = "";
+  };
+  console.keyMap = "uk";
+
+  # User account
+  users.users.mizutani = {
+    isNormalUser = true;
+    description = "Teague Mangan";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+  };
+
+  # System packages
+  environment.systemPackages = with pkgs; [
+  ];
+
+  # SSH
+  services.openssh = {
+    enable = true;
+    ports = [ 6622 ];
+    settings = {
+      PasswordAuthentication = false;
+      PermitRootLogin = "no";
+    };
+  };
+
+  # Firewall
+  networking.firewall.allowedTCPPorts = [ 6622 ];
+
+  # Fail2ban
+  services.fail2ban.enable = true;
+
+  system.stateVersion = "25.11";
+}
